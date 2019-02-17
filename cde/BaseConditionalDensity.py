e25e52f7544827601a7236d1daeae83e37906660
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2019-02-16 20:44:18 -0800

    estimator release preps

--- cde/BaseConditionalDensity.py
+++ cde/BaseConditionalDensity.py
@@ -1,6 +1,6 @@
 from sklearn.base import BaseEstimator
 
-from cde.utils.integration import mc_integration_student_t, numeric_integation
+origfrom cde.utils.integration import mc_integration_student_t, numeric_integation
 from cde.utils.center_point_select import *
 import scipy.stats as stats
 import matplotlib as mpl
